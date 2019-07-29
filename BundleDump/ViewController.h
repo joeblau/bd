ebfaf56665a41d95e4cea3aef0098f75737464f2
Joe Blau <josephblau@gmail.com>
2019-07-29 12:46:34 -0400

    Initial Commit

---
+++ BundleDump/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  BundleDump
+//
+//  Created by Joe Blau on 7/29/19.
+//  Copyright © 2019 Joe Blau. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ BundleDump/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  BundleDump
+//
+//  Created by Joe Blau on 7/29/19.
+//  Copyright © 2019 Joe Blau. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
